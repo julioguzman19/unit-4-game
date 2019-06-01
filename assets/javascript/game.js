a064c414f086e5fb545a12b8f2764fbf7c376b65
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-01 13:42:20 -0600

    Jquery initilization

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,17 @@
 
-    function myFunction() {
-        $("#image1Number").html(5);
-      }
+$(document).ready(function() {
+
+    $("#image1Number").on('click',function(){
+      console.log(5);
+      let a=6;
+      $("#test").html(a); //populates 6 where test is the tag
+    });
+
+
+})
 
 
 
-      /// IN CLASS//
-      /*Get used to typing in*/ $(".something").on("click",function(){})
-      //"click" name of event can use hover etc. This is the first parameter
-      //function() is the second parameter can create a function and put the name in there
-      //below on click will make it execute that function
 
 
-      $(".something").on("click",function(){
-      });
 
-      //not needed right now but essentially a loop method
-      $(".something").each(function(item,id){
-
-      });
