8fd9d8c1c8c1cfc02be1d96c22911a6e3757987a
julioguzman19 <48781716+julioguzman19@users.noreply.github.com>
2019-06-01 18:03:57 -0600

    images functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,51 @@
 
 $(document).ready(function() {
 
-    $("#image1Number").on('click',function(){
-      console.log(5);
-      let a=6;
-      $("#test").html(a); //populates 6 where test is the tag
-    });
+    firstImageNumberGenerator()
 
+    secondImageNumberGenerator()
+  
+    thirdImageNumberGenerator()
 
+    fourthImageNumberGenerator()
 })
 
+function firstImageNumberGenerator(){
+  //Image1 Number
+  $("#image1Number").on('click',function(){
+    let firstNumber=Math.floor(Math.random() * Math.floor(12));;
+    $("#test").html(firstNumber); //populates 6 where test is the tag
+  });
+}
 
+function secondImageNumberGenerator(){
+  //Image2 Number
+  $("#image2Number").on('click',function(){
+    let secondNumber=Math.floor(Math.random() * Math.floor(12));;
+    $("#test").html(secondNumber); //populates 6 where test is the tag
+  });
+}
 
+function thirdImageNumberGenerator(){
+   //Image3 Number
+   $("#image3Number").on('click',function(){
+    let thirdNumber=Math.floor(Math.random() * Math.floor(12));;
+    $("#test").html(thirdNumber); //populates 6 where test is the tag
+  });
+}
+
+function fourthImageNumberGenerator(){
+    //Image4 Number
+    $("#image4Number").on('click',function(){
+      let fourthNumber=Math.floor(Math.random() * Math.floor(12));;
+      $("#test").html(fourthNumber); //populates 6 where test is the tag
+    });
+}
+
+function yourCurrentNumber(inputtedNumber){
+  let currentNewNumber = currentNewNumber + inputtedNumber;
+  console.log(currentNewnNumber);
+}
 
 
 
